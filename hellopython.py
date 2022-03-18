75160e65e638391f14159d6d097bb42fbb273dd2
seungjae00 <101385177+seungjae00@users.noreply.github.com>
2022-03-18 17:15:31 +0900

    Update hellopython.py

--- hellopython.py
+++ hellopython.py
@@ -1,5 +1,5 @@
 # SeungJae Jeon , 201803186 , Electronic Engineering
 toched by seungjae00
 print("hello world\n")
-print("SeungJae Jeon , 201803186 , Electronic Engineering")
-print("touched file")
+print("SeungJae Jeon , 201803186 , Electronic Engineering/n")
+print("touched file/n")
